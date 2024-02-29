1ce055c7773020ddc54318d284ad24730df8e9fe
brk-cn <brkcndev@gmail.com>
2024-02-29 23:50:19 +0300

    add some features

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
+const path = require("path");
+const http = require("http");
 const express = require("express");
-const socketIO = require("socket.io");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/message");
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require("./utils/user");
 
 const app = express();
-const server = app.listen(3000);
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static("public"));
-
-const io = socketIO(server);
+const server = http.createServer(app);
+const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log(`${socket.id} connected`);
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    socket.emit("message", formatMessage("SERVER", "Welcome to Real-Time Chat App"));
+
+    socket.broadcast.to(user.room).emit("message", formatMessage("SERVER", `${user.username} has joined the room`));
+
+    io.to(user.room).emit("roomUsers", { room: user.room, users: getRoomUsers(user.room) });
+  });
+
+  socket.on("chatMessage", (message) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit("message", formatMessage(user.username, message));
+  });
 
   socket.on("disconnect", () => {
-    console.log(`${socket.id} disconnected`);
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", formatMessage("SERVER", `${user.username} has left the room`));
+      io.to(user.room).emit("roomUsers", { room: user.room, users: getRoomUsers(user.room) });
+    }
   });
+});
 
-  socket.on("chat", (data) => {
-    io.emit("chat", data);
-  });
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
